db4afc4e347363ba3b7165999f87dddbec13261a
mdreame <1067774901@qq.com>
2023-06-26 22:23:27 +0800

    1.2 bounce ball program done

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+def init_bounce(height, times):
+    which_time = 1
+    while which_time <= times:
+        height = round(height * 3 / 5, 4)
+        print(which_time, height)
+        which_time += 1
+
+init_bounce(100, 10)
