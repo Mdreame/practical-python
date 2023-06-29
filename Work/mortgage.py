bba34d2aa54ce5f233e1b075dbb6e93bfcbd57ec
mdreame <1067774901@qq.com>
2023-06-29 14:05:45 +0800

    1.9 done

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,28 +1,27 @@
 # mortgage.py
 #
-# Exercise 1.7
+# Exercise 1.8
 principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
-times = 0
+months = 1
+
+    #默认参数的使用
+def set_extra_payment(start, end, extra, payment=2684.11):
+    if start < months < end:
+        payment = payment + extra
+
+    return  payment
 
 while principal > 0:
+    payment = set_extra_payment(61, 108, 1000)
 
-    # 难点在于区分何时需要循环、合适需要判断
-    # 此处不需要对前12个月建立倒数计时
-    if times < 12:
-        # payment = payment  + 1000
-        # 这是不对的，每次更新都会改变全局变量payment
-        payment = 3684.11
-    else:
-        payment = 2684.11
-
-    print(payment, times)
+    # print(payment, months)
     principal = principal * (1+rate/12) - payment
     total_paid = total_paid + payment
-    
-    times = times + 1
+
+    months = months + 1
 
 print('Total paid', total_paid)
 
