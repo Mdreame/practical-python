70b7dcab9193bf40273ab2bac9b90ec59c7b1a0a
mdreame <1067774901@qq.com>
2023-06-30 18:59:29 +0800

    Introduction: 1.27 read a csv file done

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,12 @@
 # pcost.py
 #
 # Exercise 1.27
+with open('Data/portfolio.csv', 'rt') as f:
+    header = next(f)
+    total = 0
+
+    for line in f:
+        row = line.split(',')
+        total = total + int(row[1]) * float(row[2])
+
+print('Total cost', total)
