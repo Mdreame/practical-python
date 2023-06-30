cea785ff9c4824429c95c66277bd9902e0604d07
mdreame <1067774901@qq.com>
2023-06-30 19:30:58 +0800

    Introduction: function 1.30 done

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,12 +1,15 @@
 # pcost.py
 #
 # Exercise 1.27
-with open('Data/portfolio.csv', 'rt') as f:
-    header = next(f)
-    total = 0
+def portfolio_cost(file):
+    with open(file, 'rt') as f:
+        header = next(f)
+        total = 0
 
-    for line in f:
-        row = line.split(',')
-        total = total + int(row[1]) * float(row[2])
+        for line in f:
+            row = line.split(',')
+            total = total + int(row[1]) * float(row[2])
 
-print('Total cost', total)
+    return total
+cost = portfolio_cost('Data/portfolio.csv')
+print('Total cost', cost)
