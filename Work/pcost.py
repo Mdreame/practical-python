34aa9f68844974eb0b205b49b32a1c455093b2c9
mdreame <1067774901@qq.com>
2023-06-30 19:44:38 +0800

    Introduction: Function Error handling 1.31 done

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,14 +2,20 @@
 #
 # Exercise 1.27
 def portfolio_cost(file):
-    with open(file, 'rt') as f:
-        header = next(f)
-        total = 0
+    try:
+        with open(file, 'rt') as f:
+            next(f)
+            total = 0
 
-        for line in f:
-            row = line.split(',')
-            total = total + int(row[1]) * float(row[2])
+            for line in f:
+                row = line.split(',')
+                total = total + int(row[1]) * float(row[2])
 
-    return total
+        return total
+    # error name must match the error type
+    except FileNotFoundError:
+        print('no such file: %s' % file)
+
+
 cost = portfolio_cost('Data/portfolio.csv')
 print('Total cost', cost)
