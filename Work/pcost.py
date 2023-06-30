93da85679c90326dcdd3166bdeb9b09787bc62ed
mdreame <1067774901@qq.com>
2023-06-30 19:52:43 +0800

    Introduction: Function use csv module 1.32 done

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,21 +1,22 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
+
 def portfolio_cost(file):
     try:
         with open(file, 'rt') as f:
             next(f)
             total = 0
-
-            for line in f:
-                row = line.split(',')
+            rows = csv.reader(f)
+            
+            for row in rows:
                 total = total + int(row[1]) * float(row[2])
 
         return total
     # error name must match the error type
     except FileNotFoundError:
         print('no such file: %s' % file)
-
 
 cost = portfolio_cost('Data/portfolio.csv')
 print('Total cost', cost)
